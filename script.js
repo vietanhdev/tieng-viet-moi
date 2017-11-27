76a548dc87846539146c929ebfc6aa3ae64886f0
Viet Anh Nguyen <vietanh@vietanhdev.com>
2017-11-27 10:35:35 +0700

    complete converting method & add Vietnamese checking method

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+// Convert map from old Vietnamese to new Vietnamese
+// Follow the method of Associate Professor Bui Hien
+// https://news.zing.vn/toan-bo-de-xuat-cai-tien-phu-am-tieq-viet-cua-pgs-bui-hien-post799107.html
 var convertMap = [
 	['kh', 'x'],
 	['c(?!h)', 'k'],
@@ -17,25 +20,31 @@ var convertMap = [
 	['nh', 'n\'']
 ];
 
+// Textbox for original text
 var originTextArea = $('#origin-text');
+
+// Textbox for converted text
 var resultTextArea = $('#converted-text');
+
+// Copy button used for copying result
 var copyBtn = document.querySelector('#copy-result');
 
 
-// Function to convert string to new Vietnamese style
+// Function to convert storring to new Vietnamese style
 function newVietnameseStyle(str, convertMap) {
     for (let i = 0; i < convertMap.length; i++) {
 		const element = convertMap[i];
-		const searchRegEx = new RegExp(element[0], 'gi')
-		var subStrIndex;
+		const searchRegEx = new RegExp(element[0], 'i');
 		subStrIndex = str.search(searchRegEx);
 
 		while(subStrIndex != -1) {
+
 			if (str[subStrIndex] == str[subStrIndex].toUpperCase()) {
 				str = str.replace(searchRegEx, element[1].toUpperCase());
 			} else {
 				str = str.replace(searchRegEx, element[1]);
 			}
+
 			subStrIndex = str.search(searchRegEx);
 		}
        
@@ -43,10 +52,89 @@ function newVietnameseStyle(str, convertMap) {
     return str;
 }
 
+var wB = ["ngh", "ng", "kh", "nh", "ph", "gh", "ch", "th", "tr", "b", "c", "d", "đ", "g", "h", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x"];
+var wE = ["c", "ch", "m", "n", "ng", "nh", "p", "t"];
+var wM_RegEx = /[ueoaiưăạảấầẩẫậắằẳẵặẹẻẽềềểễệỉịọỏốồổỗộớờởỡợụủứừÝửữựỳỵỷỹ]+/i;
+// Check if the string is Vietnamese
+function isVietnamese(str) {
+
+	// Remove prefix
+	for (let i = 0; i < wB.length; i++) {
+		if (new RegExp("^" + wB[i],"i").test(str)) {
+			str = str.substr(wB[i].length);
+			break;
+		}
+	}
+
+	// Remove suffix
+	for (let i = 0; i < wE.length; i++) {
+		if (new RegExp(wE[i] + "$","i").test(str)) {
+			str = str.substr(0, str.length - wB[i].length);
+			break;
+		}
+	}
+
+	if (str.length == 0) {
+		return 1;
+	}
+
+	return wM_RegEx.test(str);
+}
+
 
 function doConversion() {
 	var originText = originTextArea.val();
-	resultTextArea.val(newVietnameseStyle(originText, convertMap));
+
+	// Regexp to determine a Vietnamese word
+	let wordRegEx = new RegExp("[a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]");
+
+	var begin; // begin of a word;
+	var endBound = 0; // end bound of a word
+	var x = 0; // begin of considered substring
+
+	begin = 0;
+
+	var result = "";
+	var word = "";
+
+	while (begin < originText.length) {
+		
+		// Find a word
+		while (begin < originText.length && !wordRegEx.test(originText.substr(begin,1))) {
+			begin++;
+		}
+		
+		
+		// No more word
+		if (begin >= originText.length) {
+			result += originText.substr(x);
+			break;
+		}
+
+
+		// Determine the end bound of word
+		endBound = begin;
+		do {
+			endBound++;
+		} while (endBound < originText.length && wordRegEx.test(originText.substr(endBound, 1)));
+
+		
+		word = originText.substr(begin, endBound-begin);
+		
+		// Only convert if word is Vietnamese
+		if (isVietnamese(word)) {
+			word = newVietnameseStyle(word, convertMap);
+		}
+
+		result += originText.substr(x, begin - x);
+		result += word;
+
+		begin = endBound;
+		x = endBound;	
+
+	}
+
+	resultTextArea.val(result);
 }
 
 
@@ -63,6 +151,8 @@ copyBtn.addEventListener('click', function(event) {
 });
 
 
+
+// Realtime converting old Vietnamese to new Vietnamese
 originTextArea.bind('input propertychange', function() {
 	doConversion();
 });
@@ -72,7 +162,6 @@ originTextArea.bind('input propertychange', function() {
 originTextArea.val("LUẬT GIÁO DỤC.\n"
 +"Điều 7. Ngôn ngữ dùng trong nhà trường và cơ sở giáo dục khác; dạy và học tiếng nói, chữ viết của dân tộc thiểu số; dạy ngoại ngữ.\n"
 +"1. Tiếng Việt là ngôn ngữ chính thức dùng trong nhà trường và cơ sở giáo dục khác. Căn cứ vào mục tiêu giáo dục và yêu cầu cụ thể về nội dung giáo dục, Thủ tướng chính phủ quy định việc dạy và học bằng tiếng nước ngoài trong nhà trường và cơ sở giáo dục khác....\n");
-
 doConversion();
 
 });
